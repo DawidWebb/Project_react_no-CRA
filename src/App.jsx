115aef049c8aaa47f23ee1fc7094024248bf70c2
DawidWebb <tomasz.matras@developerweb.pl>
2020-10-25 21:22:52 +0100

    feat: app store and request helper implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
+import StoreProvider from "./store/StoreProvider";
+
 import "./App.scss";
 
-const App = () => <header>Hello</header>;
+const App = () => (
+  <StoreProvider>
+    <header>Hello</header>
+  </StoreProvider>
+);
 
 export default App;
