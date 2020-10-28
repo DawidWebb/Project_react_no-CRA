263288a59c0e7df2642423323035044b1b888618
DawidWebb <tomasz.matras@developerweb.pl>
2020-10-28 21:58:57 +0100

    feat: Add buy Button for staff

--- src/components/Course/Course.jsx
+++ src/components/Course/Course.jsx
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useHistory } from "react-router-dom";
 import bemCssModules from "bem-css-modules";
 
 import { default as CourseStyles } from "./Course.module.scss";
+import request from "../../helpers/request";
+import { StoreContext } from "../../store/StoreProvider";
 
 const style = bemCssModules(CourseStyles);
 
-const Course = ({ authors, img, price, title }) => {
+const Course = ({ authors, id, img, isUserContext = false, price, title }) => {
+  const { user, setUser } = useContext(StoreContext);
+  const history = useHistory();
+
   const allAuthors = authors.join(", ");
+
+  const isUserLogged = Boolean(user);
+
+  const handleOnClick = async () => {
+    try {
+      const { data, status } = await request.patch("/users", {
+        login: user.login,
+        courseId: id,
+      });
+
+      if (status === 202) {
+        setUser(data.user);
+        history.push("/my-staffy");
+      }
+    } catch (error) {
+      console.worn(error);
+    }
+  };
+  const sholudBeBuyButtonVisible = isUserLogged && !isUserContext;
   return (
     <li>
       <article className={style()}>
@@ -14,6 +39,9 @@ const Course = ({ authors, img, price, title }) => {
         <img src={img} alt={title} className={style("image")} />
         <p className={style("price")}>{`Koszt: ${price} eur`}</p>
         <p className={style("authors")}>{`Autorzy: ${allAuthors}`}</p>
+        {sholudBeBuyButtonVisible && (
+          <button onClick={handleOnClick}>Kup</button>
+        )}
       </article>
     </li>
   );
--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -13,7 +13,9 @@ const User = () => {
 
   const boughtCourses = courses
     .filter((course) => user.courses.includes(course.id))
-    .map((course) => <Course key={course.id} {...course} />);
+    .map((course) => (
+      <Course isUserContext={true} key={course.id} {...course} />
+    ));
 
   return (
     <section className={style()}>
